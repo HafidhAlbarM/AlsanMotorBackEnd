84738abb79b17c48b9d97a72366dcf30f5b09cbf
HafidhAlbarM <43909248+HafidhAlbarM@users.noreply.github.com>
2020-12-24 00:33:16 +0700

    tambah port email

--- controllers/c_transaksi_pemesanan.js
+++ controllers/c_transaksi_pemesanan.js
@@ -41,6 +41,9 @@ exports.getOne = (req, res) => {
 const sendEmail = (dataReq, next) => {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD
