33d1ce85877959c1f6b4bfb9bf6d44c7dc91ae94
HafidhAlbarM <43909248+HafidhAlbarM@users.noreply.github.com>
2020-12-24 00:21:34 +0700

    nambah tyoe

--- controllers/c_transaksi_pemesanan.js
+++ controllers/c_transaksi_pemesanan.js
@@ -42,6 +42,7 @@ const sendEmail = (dataReq, next) => {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
+            type: "OAuth2",
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD
         }
