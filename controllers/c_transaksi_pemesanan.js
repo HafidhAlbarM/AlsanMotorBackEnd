b252e823e0e587d234143f1a7e275bd2632c8d5f
HafidhAlbarM <43909248+HafidhAlbarM@users.noreply.github.com>
2020-12-24 00:49:38 +0700

    benerin gmail

--- controllers/c_transaksi_pemesanan.js
+++ controllers/c_transaksi_pemesanan.js
@@ -40,12 +40,14 @@ exports.getOne = (req, res) => {
 
 const sendEmail = (dataReq, next) => {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        service: 'gmail',
+        secure: false,//true
+        port: 25,//465
         auth: {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD
+        }, tls: {
+            rejectUnauthorized: false
         }
     });
 
